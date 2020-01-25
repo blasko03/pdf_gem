20d9014bc9c85c3f4be9fd409fb4c60c6ae46dbb
Daniel Blasina <blzk100@gmail.com>
2020-01-25 10:30:23 +0100

    added options to renderer

--- lib/renderer.rb
+++ lib/renderer.rb
@@ -1,6 +1,8 @@
 module PdfGem 
-  ActionController::Renderers.add :pdf do |template, options|
-    send_data PdfGem::pdf_from_string({html: render_to_string(template)}), type: Mime[:pdf],  disposition: "attachment; filename=#{options[:filename]}"
+  ActionController::Renderers.add :pdf2 do |template, options|
+    params = options.except(:prefixes, :template)
+    params[:html] = render_to_string(template.present? ? template : options.template)
+    send_data PdfGem::pdf_from_string(params), type: Mime[:pdf],  disposition: "attachment; filename=#{options[:filename]}"
   end
 
   if Mime::Type.lookup_by_extension(:pdf).nil?
