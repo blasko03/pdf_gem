9272c04bf597befab4a13ff33ff08c68f83d2177
Daniel Blasina <blzk100@gmail.com>
2020-01-27 18:18:45 +0100

    v1.0.8

--- lib/renderer.rb
+++ lib/renderer.rb
@@ -1,11 +1,27 @@
-module PdfGem 
-  def self.renderer(template, options)
-    params = options.except(:prefixes, :template, :disposition, :url, :html, :filename, :formats)
-    params[:html] = render_to_string(:action => (template.present? ? template : options.template), formats: options[:formats].present? ? options[:formats] : [:pdf] )
-    send_data PdfGem::pdf_from_string(params), type: Mime[:pdf],  disposition: (params[:disposition].present? ? params[:disposition] : 'inline'), :filename => options[:filename]
+module PdfGem
+
+  class PdfGemRailtie < Rails::Railtie
+    initializer "my_railtie.configure_rails_initialization" do
+
+      ActionController::Renderers.add :pdf do |template, options|       
+        params = options.except(:prefixes, :template, :disposition, :url, :html, :filename, :formats)
+        params[:html] = render_to_string(:action => (template.present? ? template : options.template), formats: options[:formats].present? ? options[:formats] : [:pdf] )
+        send_data PdfGem::pdf_from_string(params), type: Mime[:pdf],  disposition: (params[:disposition].present? ? params[:disposition] : 'inline'), :filename => options[:filename]
+      end
+
+      ActionController::Renderers.add :pdf2 do |template, options|       
+        params = options.except(:prefixes, :template, :disposition, :url, :html, :filename, :formats)
+        params[:html] = render_to_string(:action => (template.present? ? template : options.template), formats: options[:formats].present? ? options[:formats] : [:pdf] )
+        send_data PdfGem::pdf_from_string(params), type: Mime[:pdf],  disposition: (params[:disposition].present? ? params[:disposition] : 'inline'), :filename => options[:filename]
+      end
+      if Mime::Type.lookup_by_extension(:pdf).nil?
+        Mime::Type.register('application/pdf', :pdf)
+      end
+    end
   end
+
+
+ 
   
-  if Mime::Type.lookup_by_extension(:pdf).nil?
-    Mime::Type.register('application/pdf', :pdf)
-  end
+ 
 end
